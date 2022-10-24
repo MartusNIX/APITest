83f70675dd5a0407ae25e32d4ba30b9285f22561
Daria Martus <martusdarja@gmail.com>
2022-10-24 14:16:49 +0300

    property

--- APITest/Models/EmployeeDataModel.cs
+++ APITest/Models/EmployeeDataModel.cs
@@ -7,12 +7,16 @@ namespace APITest.Models
         [JsonProperty("id")]
         public string id { get; set; }
 
+        [JsonProperty("employee_name")]
         public string employee_name { get; set; }
 
+        [JsonProperty("employee_salary")]
         public string employee_salary { get; set; }
 
+        [JsonProperty("employee_age")]
         public string employee_age { get; set; }
 
+        [JsonProperty("profile_image")]
         public string profile_image { get; set; }
     }
 }
