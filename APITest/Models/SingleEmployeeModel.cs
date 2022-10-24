707de227efa691155c0a9c36f9be1c9047889c95
Daria Martus <martusdarja@gmail.com>
2022-10-24 14:18:19 +0300

    model add

---
+++ APITest/Models/SingleEmployeeModel.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APITest.Models
+{
+    internal class SingleEmployeeModel
+    {
+        [JsonProperty("status")]
+        public string status { get; set; }
+
+        [JsonProperty("data")]
+        public ICollection<EmployeeDataModel> data { get; set; }
+    }
+}
